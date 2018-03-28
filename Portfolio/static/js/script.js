a46226fe66779daced9b05b776d82a80da31566f
migmartinez <migmart@umich.edu>
2018-03-27 23:31:58 -0400

    ice forms

---
+++ Portfolio/static/js/script.js
@@ -0,0 +1,13 @@
+function portMenu() {
+	var nav_top = document.getElementById("nav-top");
+	var menu_icon = document.getElementById("toggle-menu");
+
+	if (nav_top.className == "nav-top"){
+		nav_top.className += "toggle-open";
+		menu_icon.innerHTML = "&#10005; Close Menu";
+	} else {
+		nav_top.className = "nav-top";
+		menu_icon.innerHTML = "&#9776; Menu";
+	}
+
+}
